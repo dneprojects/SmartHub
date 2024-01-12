48f749e17f190ddc1292cfa3dabf013980d5eda5
dneprojects <dne.projects.1@gmail.com>
2024-01-12 12:40:15 +0100

    Setup in SmartCenter under VSC

--- install/run_pip_python_install.sh
+++
@@ -1,3 +0,0 @@
-cd /home/dietm/SmartHub || exit
-su -s /bin/bash -c "/usr/local/bin/pip3.11 install -r ./requirements.txt" dietm
-su -s /bin/bash -c "/usr/local/bin/python3.11  /home/dietm/SmartHub/smarthub.py > startup.log 2> err.log" dietm
