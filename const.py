94ef38f567e92aedbd12ffb488e8f75747d331ac
dneprojects <dne.projects.1@gmail.com>
2024-06-26 15:39:35 +0200

    v1.5.0

--- const.py
+++ const.py
@@ -40,7 +40,7 @@ AUTOMATIONEDIT_TEMPLATE_FILE = "automation_edit_template.html"
 class SMHUB_INFO:
     """Holds information."""
 
-    SW_VERSION = "1.4.0"
+    SW_VERSION = "1.5.0"
     TYPE = "Smart Hub"
     TYPE_CODE = "20"
     SERIAL = "RBPI"
